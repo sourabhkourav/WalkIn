93f1877712c4e1853ddcc0b8b9a13e9f43420281
Sourabh Kourav <sourabhkourav422@gmail.com>
2024-12-27 03:37:11 +0530

    Initial commit

---
+++ src/main/java/com/walkin/WalkinApplication.java
@@ -0,0 +1,13 @@
+package com.walkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalkinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WalkinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/walkin/WalkInApplicationTests.java
@@ -0,0 +1,13 @@
+package com.walkin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalkInApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
