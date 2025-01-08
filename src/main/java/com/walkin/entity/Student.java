a00dde2446d8609366484f11c8acba1a36d02f75
Sourabh Kourav <sourabhkourav422@gmail.com>
2025-01-08 20:02:33 +0530

    use lombok for getters and setters
    Signed-off-by: Sourabh Kourav <sourabhkourav422@gmail.com>

--- src/main/java/com/walkin/entity/Student.java
+++ src/main/java/com/walkin/entity/Student.java
@@ -1,6 +1,8 @@
 package com.walkin.entity;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 @Table(name = "Student")
@@ -11,18 +13,28 @@ public class Student{
     @Column(name = "student_id", nullable = false)
     private Integer studentId;
     
+    @Setter
+    @Getter
     @Column(name = "first_name",length = 20, nullable = false)
     private String firstName;
     
+    @Setter
+    @Getter
     @Column(name = "last_name",length = 20, nullable = false)
     private String lastName;
     
+    @Setter
+    @Getter
     @Column(name = "email",length = 100, nullable = false, unique = true)
     private String email;
     
+    @Setter
+    @Getter
     @Column(name = "contact_number",length = 10, nullable = false, unique = true)
     private String contactNumber;
     
+    @Setter
+    @Getter
     @Column(name = "resume")
     private byte[] resume;
 }
